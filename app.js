4e919a647cb6eb3de483552791d7ceddb3d51804
Vatsal Srivastava <srivastavavatsal548@gmail.com>
2025-06-02 18:28:30 +0530

    Commits for the RESTful APIs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
 
 const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
 main()
@@ -18,14 +19,60 @@ async function main() {
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root.");
 });
+//Index Route
 
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
   res.render("./listings/index.ejs", { allListings });
+});
+
+//NEW ROUTE
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+//SHOW ROUTE
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
+
+//CREATE ROUTE
+app.post("/listings", async (req, res) => {
+  // let listing = req.body.listing;
+  // console.log(listing);
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+});
+
+//EDIT ROUTE
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//UPDATE ROUTE
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//DELETE ROUTE
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
+  res.redirect("/listings");
 });
 
 // app.get("/testListing", async (req, res) => {
